f4a0ffc7eb989d22b73ea3da62bf0f29d7592fec
Adam Older <adamolder10@gmail.com>
2021-11-15 20:24:37 -0800

    refactor

--- src/App.js
+++ src/App.js
@@ -1,66 +1,95 @@
-import React, { Component } from 'react'
-import Header from './components/Header';
-import TodoForm from './components/todo-components/TodoForm';
-import Todos from './components/todo-components/Todos';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import TodoForm from "./components/todo-components/TodoForm";
+import Todos from "./components/todo-components/Todos";
 
-import './App.css';
-import { nanoid } from 'nanoid';
+import "./App.css";
+import { nanoid } from "nanoid";
 // import TodoItem from './components/todo-components/TodoItem';
 
 export default class App extends Component {
+  savedTodos = localStorage.getItem("todos");
+
   state = {
     todos: [
       {
         id: 1,
-        title: 'Take out the trash',
-        completed: false
+        title: "Take out the trash",
+        completed: false,
       },
       {
         id: 2,
-        title: 'Learn React',
-        completed: false
+        title: "Learn React",
+        completed: true,
       },
       {
         id: 3,
-        title: 'Create todo app',
-        completed: false
-      }
-    ]
+        title: "Create todo app",
+        completed: true,
+      },
+    ],
+  };
+  // const [state, setstate] = useState([]);
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem("todos", temp);
+    }
+  }
+  componentDidMount() {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
   }
 
   toggleComplete = (id) => {
-    this.setState({ todos: [...this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    })] });
-}
+    this.setState({
+      todos: [
+        ...this.state.todos.map((todo) => {
+          if (todo.id === id) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        }),
+      ],
+    });
+  };
 
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo => {
-      // if (todo.id !== id) return todo;
-      return todo.id !== id;
-    })] });
-  }
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          // if (todo.id !== id) return todo;
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
   submitTodo = (title) => {
     const newTodo = {
       id: nanoid(10),
       title,
-      completed:false,
+      completed: false,
     };
-    this.setState({ todos: [...this.state.todos, newTodo]})
-  }
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <TodoForm submitTodo={this.submitTodo}/>
-          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo} />
+          <TodoForm submitTodo={this.submitTodo} />
+          <Todos
+            todos={this.state.todos}
+            toggleComplete={this.toggleComplete}
+            delTodo={this.delTodo}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
-
